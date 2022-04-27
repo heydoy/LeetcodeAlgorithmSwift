d6976d1f0a0222fba7ab3606b953abba4092b54c
Kim Doy <heydoyhey@gmail.com>
2022-04-28 02:01:24 +0900

    Time: 79 ms (39.29%), Space: 14.6 MB (36.98%) - LeetHub

---
+++ 1-two-sum/1-two-sum.swift
@@ -0,0 +1,17 @@
+import Foundation
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+       let size = nums.count 
+        
+        for i in 1..<size {
+            let first = nums[size-i]
+            for j in 0..<size-i {
+                let second = nums[j]
+                if first + second == target {
+                    return [size-i, j] 
+                } 
+            }
+        } 
+        return []  
+    }
+}
